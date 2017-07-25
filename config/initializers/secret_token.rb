1e2b3e3d90e9108b091ad3349d41766e847c6d41
Jules Wittock <wittockjules@gmail.com>
2017-07-25 16:14:21 +0200

    Starting awesome development with Rails :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listgames::Application.config.secret_token = '304bae55b729b7cdb8403dea3c0284aa99950f53d7c79e637327a5eb165e0f3990b6093cc7ab22502d4c4dfbb27b3e822943f38f5ecfa1ad940d2144d7b0954b'

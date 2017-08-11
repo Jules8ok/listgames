e51a8dd22fd83895ae5db1cde70a13b0816de28e
Jules Wittock <wittockjules@gmail.com>
2017-08-11 16:01:06 +0200

    push without msg welcome confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
-  after_create :send_welcome_email
+
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
